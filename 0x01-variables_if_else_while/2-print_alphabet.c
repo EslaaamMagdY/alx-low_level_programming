19d365078ca11070fcfcda4f9429637c605514d3
EslaaamMagdY <eslaam.elsayed@outlook.com>
2023-06-19 15:49:07 -0700

    prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char a1p [26] = "absawesdfasfasddasd";
+	int i;
+	
+	for ( i=0; i < 26; i++)
+	{
+		putchar(a1p[i]);
+	}
+	putcher("\n";
+			return (0);
+			}
+
