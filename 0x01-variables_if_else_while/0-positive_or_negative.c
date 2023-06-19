dfdf0c89099a1976c362d4f286368737b6acc5a4
EslaaamMagdY <eslaam.elsayed@outlook.com>
2023-06-19 10:45:37 -0700

    determine if number is + or -

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,31 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main
- ro-
- * 
- * return: Always 0 (Success)
- */
 
+/**
+ * main - determine if number is + or -
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
+        int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-	{ printf("%d  is positive\n",n);
-	}
-	else if ( n == 0)
-	{
-		printf( "%d is zero \n" , n);
-	}
-	else
-	{ printf("%d is negitive \n" ,n );
-	}
-	return (0)
+        if (n > 0)
+        {
+                printf("%d is positive\n", n);
+        }
+        else if (n == 0)
+        {
+                printf("%d is zero\n", n);
+        }
+        else
+        {
+                printf("%d is negative\n", n);
+        }
+
+        return (0);
 }
